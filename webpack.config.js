d0ff30096854591f29b65c9d6fe51b2d34dcbcb6
Nami Honda <h@parachute.bz>
2023-11-10 18:09:24 +0900

    1110コミット

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackplugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   //エントリーポイントを登録
   //読み込むファイル名
   entry: './src/javascripts/main.js',
@@ -13,34 +14,71 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'javascripts/main.js'
   },
+  //modeの指定
+  mode: 'development', 
   module: {
     rules: [
       {
-        test: /\.css/,
+        test:/\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env',{'targets':'>0.25%,not dead'}]
+              ],
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader'
-          }
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                 require('autoprefixer')({ grid: true })
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader'
+          },
         ]
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name][ext]'
+          filename: 'images/[name][ext]',
         },
         use: [
           // {
           //   loader: 'file-loader',
-          //     options: {
-          //       esModule: false,
-          //       name: 'images/[name].[ext]',
+          //   options: {
+          //     esModule: false,
+          //     name: 'images/[name].[ext]',
           //   },
-          // }
-        ]
+          // },
+
+          // 追加
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
       },
       {
         test: /\.pug/,
@@ -58,6 +96,10 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+  },
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: './stylesheets/main.css'
@@ -70,6 +112,12 @@ module.exports = {
       template: './src/templates/access.pug',
       filename: 'access.html',
     }),
+    new HtmlWebpackplugin({
+      template: './src/templates/members/taro.pug',
+      filename: 'members/taro.html',
+    }),
     new CleanWebpackPlugin(),
-  ] 
+  ] ,
+
 }
+
